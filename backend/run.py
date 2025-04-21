d17c66fc737dee8436d6e84372b5d9271a8b7d42
AnaRecio <anarecio.89@gmail.com>
2025-04-20 21:40:28 -0600

    preparing for deployment

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 from app.main import app
 
 if __name__ == '__main__':
-    
-    app.run(debug=True)
+    from app.main import app
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
